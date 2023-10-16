17e5860f8c67d18db16702c9e78141e2d7e67be5
Mubarak1010 <anoze941@gmail.com>
2023-10-16 03:59:42 -0700

    first commit

---
+++ models/base_model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""project's module"""
+
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """The class declaration"""
+
+    def __init__(self, *args, **kwargs):
+        """Instantiating the class."""
+
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f')
+
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """A toString method"""
+
+        return "[{}] ({}) {}".format(self.__class__.__name__,self.id,self.__dict__)
+
+    def save(self):
+        """Method that updates"""
+
+        updated_at = datetime.now()
+
+    def to_dict(self):
+        """Dictionary rep of the instance"""
+
+        new_dict = dict(self.__dict__)
+        new_dict['created_at'] = self.__dict__['created_at'].isoformat()
+        new_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        return(new_dict)
